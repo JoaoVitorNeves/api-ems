664f16af9c2d1792554a6f696628040e445acc81
JoaoVitorNeves <jvitornevessilva@gmail.com>
2023-11-08 00:18:58 -0300

    Add the function to update Employees

--- ems-backend/src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -40,4 +40,17 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employees.stream().map( (employee) -> EmployeeMapper.mapToEmployeeDto(employee))
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updateEmployee) {
+
+       Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee is not exist with give id :"+ employeeId)
+        );
+       employee.setFirstName(updateEmployee.getFirstName());
+       employee.setLastName(updateEmployee.getLastName());
+       employee.setEmail(updateEmployee.getEmail());
+       Employee updateEmployeeObj =  employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updateEmployeeObj);
+    }
 }
